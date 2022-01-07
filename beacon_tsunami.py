8d8f0d213d9f120b83431c66e772e4fcb354968b
said4244 <69765135+said4244@users.noreply.github.com>
2022-01-07 13:20:37 -0500

    Update beacon_tsunami.py

--- beacon_tsunami.py
+++ beacon_tsunami.py
@@ -9,19 +9,20 @@ class CreateBeacon:
     #info for frame
     self.ssid = ssid
     self.number = number
-    self.addr = RandMAC()
+    self.addr = RandMAC() #for random generated mac address
     self.iface = 'wlan0mon'
 
     self.dot11 = Dot11(type=0, subtype=8, 
     addr1='ff:ff:ff:ff:ff:ff', 
     addr2 = self.addr,
     addr3 = self.addr)
+    #ff:ff:ff:ff:ff:ff is the broadcast address
 
     #Beacon layer
-    self.beacon = Dot11Beacon(cap='ESS+privacy')
+    self.beacon = Dot11Beacon(cap='ESS+privacy') # this line assures that iphones see our fake ap's as secure
 
     #Information Element
-    self.essid = Dot11Elt(ID='SSID', info=self.ssid, len=len(self.ssid))
+    self.essid = Dot11Elt(ID='SSID', info=self.ssid, len=len(self.ssid)) #this assures that our AP's show up as secure and don't get out filtered by some random protocol
     self.rsn = Dot11Elt(ID='RSNinfo', info=(
     '\x01\x00'
     '\x00\x0f\xac\x02'
@@ -33,9 +34,9 @@ class CreateBeacon:
     '\x00\x00'))
 
     #all layers stacked
-    self.frame = RadioTap()/self.dot11/self.beacon/self.essid/self.rsn
+    self.frame = RadioTap()/self.dot11/self.beacon/self.essid/self.rsn  #the beacon frame
   def Send(self):
-    sendp(self.frame, inter=0.050, iface=self.iface, loop=1)
+    sendp(self.frame, inter=0.050, iface=self.iface, loop=1) #sending the beacon frame
 
 
 
@@ -62,8 +63,8 @@ class MultiProcessBeacon:
       # i = multiprocessing.Process(target=SendBeacon.Send, args=Beacon.frame)
       for _ in range(3):    #sending out the same beacon 3 times because for some reason sending only 1 beacon does not always work
         try:
-          i = multiprocessing.Process(target=Beacon.Send)
-          i.start()
+          i = multiprocessing.Process(target=Beacon.Send) #creating a new process
+          i.start() #starting the new process
         except KeyboardInterrupt:
           print('processes stopped')
           time.sleep(1)
@@ -91,13 +92,13 @@ class InputMain():
     for n in range(int(input_number)):
       n += 1 #because it starts with 0
       ask_ssid = input('Name SSID for AP number ' + str(n)+': ')
-
+      #ssid length handeling
       if len(ask_ssid) > 32:
         print('Maximum length of ssid exceeded.')
         ask_ssid = ask_ssid[32]
       input_ssid.append(ask_ssid)
 
-    tuple(input_ssid)
+    tuple(input_ssid) #typling it cause why not
     
     self.given_number = int(input_number)
     self.given_ssid = input_ssid
